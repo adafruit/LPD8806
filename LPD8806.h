8dcd73608e48548ba245214c5f6f3b21dbe4f8ba
Paint Your Dragon <paintyourdragon@dslextreme.com>
2011-12-01 17:44:37 -0800

    Added longstrandtest sketch and extra debug modes

--- LPD8806.h
+++ LPD8806.h
@@ -1,29 +1,43 @@
-#if (ARDUINO <= 22)
- #include <WProgram.h>
-#else
+#if (ARDUINO >= 100)
  #include <Arduino.h>
+#else
+ #include <WProgram.h>
 #endif
 
 class LPD8806 {
- private:
-  void write8(byte);
-  // the arrays of bytes that hold each LED's 24 bit color values
-  uint8_t *pixels;
-  uint16_t numLEDs;
-  uint8_t dataPin, clockPin;
-  volatile uint8_t *clkportreg, *mosiportreg;
-  uint8_t clkpin, mosipin;
-  boolean hardwareSPI;
-
-  void writezeros(uint16_t n);
 
  public:
+
   LPD8806(uint16_t n, uint8_t dpin, uint8_t cpin);
   LPD8806(uint16_t n);
-  void begin();
-  void show();
-  void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b);
-  void setPixelColor(uint16_t n, uint32_t c);
-  uint16_t numPixels(void);
-  uint32_t Color(byte, byte, byte);
+  void
+    begin(void),
+    show(void),
+    setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b),
+    setPixelColor(uint16_t n, uint32_t c);
+  uint16_t
+    numPixels(void);
+  uint32_t
+    Color(byte, byte, byte);
+
+  boolean
+    slowmo; // If true, use digitalWrite instead of direct PORT writes
+  uint8_t
+    pause;  // Delay (in milliseconds) after latch
+
+ private:
+
+  uint8_t
+    *pixels;     // Holds LED color values
+  uint16_t
+    numLEDs;     // Number of RGB LEDs in strand
+  boolean
+    hardwareSPI; // If true, using hardware SPI, the following are ignored:
+  uint8_t
+    datapin, datapinmask, clockpin, clockpinmask;
+  volatile uint8_t
+    *clockport, *dataport;
+
+  void
+    writezeros(uint16_t n);
 };
