4836d44f28e51b3bc2fddb6c299f7a800cd86450
Michael Vogt <michu@neophob.com>
2011-12-28 22:18:39 +0100

    add gamma tab, remove unused slowmo mode (I need each byte)

--- LPD8806.cpp
+++ LPD8806.cpp
@@ -1,12 +1,52 @@
+#include <avr/pgmspace.h>
 #include "LPD8806.h"
 #include "SPI.h"
-#include "pins_arduino.h"
 
 // Arduino library to control LPD8806-based RGB LED Strips
 // (c) Adafruit industries
 // MIT license
 
 /*****************************************************************************/
+
+// Gamma correction compensates for our eyes' nonlinear perception of
+// intensity.  It's the LAST step before a pixel value is stored, and
+// allows intermediate rendering/processing to occur in linear space.
+// The table contains 256 elements (8 bit input), though the outputs are
+// only 7 bits (0 to 127).  This is normal and intentional by design: it
+// allows all the rendering code to operate in the more familiar unsigned
+// 8-bit colorspace (used in a lot of existing graphics code), and better
+// preserves accuracy where repeated color blending operations occur.
+// Only the final end product is converted to 7 bits, the native format
+// for the LPD8806 LED driver.  Gamma correction and 7-bit decimation
+// thus occur in a single operation.
+PROGMEM prog_uchar gammaTable[]  = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,
+    2,  2,  2,  2,  2,  3,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,
+    4,  4,  4,  4,  5,  5,  5,  5,  5,  6,  6,  6,  6,  6,  7,  7,
+    7,  7,  7,  8,  8,  8,  8,  9,  9,  9,  9, 10, 10, 10, 10, 11,
+   11, 11, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 15, 15, 16, 16,
+   16, 17, 17, 17, 18, 18, 18, 19, 19, 20, 20, 21, 21, 21, 22, 22,
+   23, 23, 24, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 30,
+   30, 31, 32, 32, 33, 33, 34, 34, 35, 35, 36, 37, 37, 38, 38, 39,
+   40, 40, 41, 41, 42, 43, 43, 44, 45, 45, 46, 47, 47, 48, 49, 50,
+   50, 51, 52, 52, 53, 54, 55, 55, 56, 57, 58, 58, 59, 60, 61, 62,
+   62, 63, 64, 65, 66, 67, 67, 68, 69, 70, 71, 72, 73, 74, 74, 75,
+   76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91,
+   92, 93, 94, 95, 96, 97, 98, 99,100,101,102,104,105,106,107,108,
+  109,110,111,113,114,115,116,117,118,120,121,122,123,125,126,127
+};
+
+// This function (which actually gets 'inlined' anywhere it's called)
+// exists so that gammaTable can reside out of the way down here in the
+// utility code...didn't want that huge table distracting or intimidating
+// folks before even getting into the real substance of the program, and
+// the compiler permits forward references to functions but not data.
+inline byte gamma(byte x) {
+  return pgm_read_byte(&gammaTable[x]);
+}
+
 
 // Constructor for bitbanged (software) SPI:
 LPD8806::LPD8806(uint16_t n, uint8_t dpin, uint8_t cpin) {
@@ -21,11 +61,11 @@ LPD8806::LPD8806(uint16_t n, uint8_t dpin, uint8_t cpin) {
     clockpin     = cpin;
     clockpinmask = digitalPinToBitMask(cpin);
     clockport    = portOutputRegister(digitalPinToPort(cpin));
-    slowmo       = false;
     pause        = 3;
   }
 }
 
+#ifdef HARDWARE_SPI
 // Constructor for hardware SPI use:
 LPD8806::LPD8806(uint16_t n) {
   // Allocate 3 bytes per pixel:
@@ -36,9 +76,11 @@ LPD8806::LPD8806(uint16_t n) {
     pause       = 3;
   }
 }
+#endif
 
 // Empty Constructor, used only as placeholder, library will be initialized later
 LPD8806::LPD8806() {
+
 }
 
 void LPD8806::begin(void) {
@@ -52,8 +94,7 @@ void LPD8806::begin(void) {
   } else {
     pinMode(datapin , OUTPUT);
     pinMode(clockpin, OUTPUT);
-    if(slowmo) digitalWrite(clockpin, LOW);
-    else       *clockport &= ~clockpinmask; // Clock = low
+    *clockport &= ~clockpinmask; // Clock = low
   }
 
   // Issue initial latch to 'wake up' strip (latch length varies w/numLEDs)
@@ -67,12 +108,6 @@ uint16_t LPD8806::numPixels(void) {
 void LPD8806::writezeros(uint16_t n) {
   if (hardwareSPI) {
     while(n--) SPI.transfer(0);
-  } else if(slowmo) {
-    digitalWrite(datapin, LOW);
-    for(uint16_t i = 8 * n; i>0; i--) {
-      digitalWrite(clockpin, HIGH);
-      digitalWrite(clockpin, LOW);
-    }
   } else {
     *dataport &= ~datapinmask; // Data low
     for(uint16_t i = 8 * n; i>0; i--) {
@@ -88,26 +123,19 @@ void LPD8806::writezeros(uint16_t n) {
 // this from a strip controller and it seems to work very nicely!
 void LPD8806::show(void) {
   uint16_t i, nl3 = numLEDs * 3; // 3 bytes per LED
-  
+  uint8_t tmp; //gamma applied temp value
+
   // write 24 bits per pixel
   if (hardwareSPI) {
     for (i=0; i<nl3; i++ ) {
-      SPDR = pixels[i];
+      SPDR = gamma(pixels[i]);
       while(!(SPSR & (1<<SPIF)));
-    }
-  } else if(slowmo) {
-    for (i=0; i<nl3; i++ ) {
-      for (uint8_t bit=0x80; bit; bit >>= 1) {
-        if(pixels[i] & bit) digitalWrite(datapin, HIGH);
-        else                digitalWrite(datapin, LOW);
-        digitalWrite(clockpin, HIGH);
-        digitalWrite(clockpin, LOW);
-      }
     }
   } else {
     for (i=0; i<nl3; i++ ) {
+      tmp = gamma(pixels[i]);
       for (uint8_t bit=0x80; bit; bit >>= 1) {
-        if(pixels[i] & bit) *dataport |=  datapinmask;
+        if(tmp & bit) *dataport |=  datapinmask;
         else                *dataport &= ~datapinmask;
         *clockport |=  clockpinmask;
         *clockport &= ~clockpinmask;
@@ -147,4 +175,6 @@ void LPD8806::setPixelColor(uint16_t n, uint32_t c) {
   pixels[n*3+1] = (c >>  8) | 0x80;
   pixels[n*3+2] =  c        | 0x80;
 }
+
+
 
